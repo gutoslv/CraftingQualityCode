57ff4e4556347680f7ca79773661594c509a1e00
Augusto Cesar <guto_slv@live.com>
2019-02-19 12:53:58 -0300

    Creates is_palindrome_v3() function.

--- Week1/palindrome.py
+++ Week1/palindrome.py
@@ -34,6 +34,29 @@ def is_palindrome_v2(s):
     return s[:n // 2] == reverse(s[n - n // 2:])
 
 
+def is_palindrome_v3(s):
+    """ (str) -> bool
+
+    Return True if and only if s is a palindrome.
+
+    >>> is_palindrome_v3('noon')
+    True
+    >>> is_palindrome_v3('racecar')
+    True
+    >>> is_palindrome_v3('dented')
+    False
+    """
+
+    i = 0
+    j = len(s) - 1
+
+    while i < j and s[i] == s[j]:
+        i = i + 1
+        j = j - 1
+
+    return j <= i
+
+
 def reverse(s):
     """ (str) -> str
 
